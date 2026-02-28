4e2b72b8f5607b72d5ebb627de1c80a44babf33b
poomrelax <poomnnn2341@gmail.com>
2026-02-28 13:16:05 +0700

    update28/2/69(2)

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,13 @@ import styles from "./page.module.css";
 import RelaxIconPNG from '@/public/poomrelaxIconPNG.png'
 import TypeWring from "@/components/TypeWring";
 import { HiChevronDoubleDown } from "react-icons/hi2";
+import Profile from "@/components/Profile/Profile";
 
 export default function Home() {
   return (
     <>
     <div className={styles.header}>
+      <div className={styles.container}>
       <div className={styles.i_backdrop}></div>
       <div className={styles.content}>
         <div style={{color: '#fff', display: 'flex', gap: '8px', fontFamily: 'var(--font1)', fontSize: 11}}><h1>welcome to</h1><h1 >website</h1></div>
@@ -17,11 +19,15 @@ export default function Home() {
           <h2>{"/>"}</h2>
         </div>
         {/* <h3 style={{fontSize: '16px', fontFamily: 'var(--font1)',  marginTop: '5px'}}>This website, belonging to Mr. Puriphat Hemtananont</h3> */}
-    <TypeWring/>
-      </div>
+       <TypeWring/>
+      </div> 
       <Image src={RelaxIconPNG} alt="poomrelaxIcon" width={300} height={300} style={{marginRight: '10%'}}/>
     </div>
-    <HiChevronDoubleDown color="#fff"/>
+    <div className={styles.scrollA}>
+      <HiChevronDoubleDown color="#ffffff" size={25}/>
+    </div>
+    </div>
+    <Profile />
     </>
   );
 }
---
+++ components/Profile/Profile.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from 'react'
+import styleProfile from './Profile.module.css'
+import { Typewriter } from 'react-simple-typewriter'
+
+function Profile() {
+  return (
+    <>
+      <div className={styleProfile.container}>
+        <div className={styleProfile.content}>
+          <div className={styleProfile.TextContent}>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Profile
