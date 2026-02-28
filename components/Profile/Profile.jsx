a6b1f385cfb952985ddb469e29d8c41b3c7f9cb1
poomrelax <poomnnn2341@gmail.com>
2026-02-28 22:16:41 +0700

    update28/2/69

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -5,12 +5,15 @@ import styleProfile from './Profile.module.css'
 import { Typewriter } from 'react-simple-typewriter'
 
 function Profile() {
+
+  const word = ['Name: Puripat Hemthanon\nNickName: Poom']
+
   return (
     <>
       <div className={styleProfile.container}>
         <div className={styleProfile.content}>
           <div className={styleProfile.TextContent}>
-            
+            <Typewriter words={word} loop={1} cursor cursorStyle="_" typeSpeed={40}/>
           </div>
         </div>
       </div>
